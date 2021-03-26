5bc0201a9f08e02af265fcbfec14f03a1ff92c27
in-in <dmin@tuta.io>
2021-03-26 22:32:16 +0300

    fix(smoothscroll): add native scrollIntoView
    bye-bye safari :c

--- src/index.js
+++ src/index.js
@@ -1,37 +1,14 @@
 import 'alpinejs';
 
 window.covid = () => ({
-	smoothScroll(id, duration = 1000) {
-		const
-			element = document.scrollingElement || document.documentElement;
-		const to = document
+	smoothScroll(id) {
+		document
 			.querySelector(id)
-			.getBoundingClientRect().top + -45;
-		const start = element.scrollTop;
-		const change = to - start;
-		const startDate = +new Date();
-
-		const easeInOutQuad = (currentTime, b, c, d) => {
-			let t = currentTime;
-			t /= d / 2;
-			if (t < 1) return (c / 2) * t * t + b;
-			t -= 1;
-			return -(c / 2) * (t * (t - 2) - 1) + b;
-		};
-		const animateScroll = () => {
-			const currentDate = +new Date();
-			const currentTime = currentDate - startDate;
-			element.scrollTop = parseInt(
-				easeInOutQuad(currentTime, start, change, duration), 10,
-			);
-			if (currentTime < duration) {
-				requestAnimationFrame(animateScroll);
-			} else {
-				element.scrollTop = to;
-			}
-		};
-		animateScroll();
+			.scrollIntoView({
+				'behavior': 'smooth',
+			});
 	},
+
 	'thresholdBottom': null,
 	'thresholdTop': null,
 
